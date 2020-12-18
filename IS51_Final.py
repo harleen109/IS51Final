1deee832c1ace184a6a9c121a86435917fbc4c8b
harleen109 <harleen109@mail.fresnostate.edu>
2020-12-17 18:11:54 -0800

    Finished strucutured-english

---
+++ IS51_Final.py
@@ -0,0 +1,11 @@
+"""
+The program will need to first read the data from the data file named Final.txt to diplay students score from the Final exam
+
+the program will need to count count all the individual grades of all students
+
+the program will need to output the class average from all the students scores
+
+the program will need to output the class average percentage of students who scored above the average which should be 54.17%
+
+"""
+
