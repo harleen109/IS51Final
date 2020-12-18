99357026accec8249794a2a7d776042412d44800
harleen109 <harleen109@mail.fresnostate.edu>
2020-12-17 18:16:40 -0800

    Finished strucutured-english

--- IS51_Final.py
+++ IS51_Final.py
@@ -7,5 +7,10 @@ the program will need to output the class average from all the students scores
 
 the program will need to output the class average percentage of students who scored above the average which should be 54.17%
 
+function1 will output data.split()
+function2 will output sum(data) / len(data)
+function3 will output count = count + 54.17
+
+
 """
 
